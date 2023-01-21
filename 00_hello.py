72da4461231feadc9a8ffa532f16e9dd400ea82e
Brian Almada <brianalexisalmada84@gmail.com>
2023-01-21 00:29:53 -0300

    Agregado de metodo type para observar tipos de datos

--- 00_hello.py
+++ 00_hello.py
@@ -1,7 +1,10 @@
 #Comenzando las prácticas con el típico HolaMundo (aunque con el ligero cambio de Hola Python)
+
 print('hola Python')
 
-
-
-
-
+#Presentación de tipos de datos que se pueden usar en Python
+print(type("soy un dato str"))
+print(type(5))
+print(type(1.5))
+print(type(3 + 1j))
+print(type(True))
