da60818db381c20425b7a44210ce3d520a647d0d
Brian Almada <brianalexisalmada84@gmail.com>
2023-01-24 10:23:29 -0300

    Agregado de variables

--- 01_variables.py
+++ 01_variables.py
@@ -2,3 +2,15 @@
 
 my_string_variable = "My String Variable"
 print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+my_bool_variable = False
+print(my_bool_variable)
+
+print(my_string_variable, my_int_to_str_variable, my_bool_variable)
