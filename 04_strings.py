df31c8c453df85bc56d5804da3c332d5ca65deeb
Brian Almada <brianalexisalmada84@gmail.com>
2023-01-31 20:58:42 -0300

    Agregando al archivo 04 sobre variables con string, tabulacion, escapado, etc

---
+++ 04_strings.py
@@ -0,0 +1,19 @@
+# Strings
+
+my_string =  'My String'
+my_other_string = "otro string"
+
+my_strings = my_string, my_other_string
+
+print(my_string + " " + my_other_string)
+print(my_string + " " + my_other_string)
+print(my_string + " y " + my_other_string)
+
+my_new_line_string = 'Este es un \n salto de línea'
+print(my_new_line_string)
+
+my_tab_string = '\t Este es un texto con tabulación'
+print(my_tab_string)
+
+my_scape_string = '\\t Este es un texto \\n con escapado'
+print(my_scape_string)
