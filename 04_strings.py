30d8e2b87dd348082f6767d202638384b3d7586d
Brian Almada <brianalexisalmada84@gmail.com>
2023-01-31 21:41:33 -0300

    Agregando al archivo 04 sobre formatear strings

--- 04_strings.py
+++ 04_strings.py
@@ -17,3 +17,11 @@ print(my_tab_string)
 
 my_scape_string = '\\t Este es un texto \\n con escapado'
 print(my_scape_string)
+
+# Formateo
+
+name, surname, age = 'Brian', 'Almada', 32
+
+print('Mi nombre es {}, mi apellido es {} y mi edad es de {} años'.format(name, surname, age))
+print('Mi nombre es %s, mi apellido es %s y mi edad es de %d años' %(name, surname, age))
+print(f'Mi nombre es {name}, mi apellido es {surname} y mi edad es de {age} años')
