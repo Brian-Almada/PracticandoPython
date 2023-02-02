f4b765ce4dd7e354d01fbc804a53822db496abd2
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-01 21:43:23 -0300

    índices y rangos

--- 04_listas.py
+++ 04_listas.py
@@ -14,3 +14,14 @@ my_other_list = [32, 1.89, 'Brian', 'Almada']
 
 print(type(my_list))
 print(type(my_other_list))
+
+#print(my_other_list[4]) IndexError: list index out of range (indice fuera de rango)
+print(my_other_list[3])
+print(my_other_list[2])
+print(my_other_list[1])
+print(my_other_list[0])
+#print(my_other_list[-5]) IndexError: list index out of range (indice fuera de rango)
+print(my_other_list[-4])
+print(my_other_list[-3])
+print(my_other_list[-2])
+print(my_other_list[-1])
