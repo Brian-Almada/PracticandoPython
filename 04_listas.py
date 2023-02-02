dd9ca3a4c818ca31571b7c80e97e3814af765d1b
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-01 21:28:38 -0300

    Teoría básica de listas

---
+++ 04_listas.py
@@ -0,0 +1,16 @@
+#Lists
+# nota: La lista ya no es una base sino una estructura
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [35, 55, 57, 88, 54, 646, 44, 88, 45]
+
+print(my_list)
+print(len(my_list))
+
+my_other_list = [32, 1.89, 'Brian', 'Almada']
+
+print(type(my_list))
+print(type(my_other_list))
