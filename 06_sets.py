3089711c3fac2d42bef53975c588ed303ca50204
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-02 23:51:59 -0300

    Agregando teoria basica de sets

--- 06_sets.py
+++ 06_sets.py
@@ -1,1 +1,17 @@
 #Sets
+# Es una colección que no posee órden, y por tanto, tampoco números de índex
+
+my_set = set()
+my_other_set = {}
+
+print(type(my_set))
+print(type(my_other_set)) # Inicialmente es un diccionario
+
+my_other_set = {'Brian', 'Almada', 32}
+print(type(my_other_set))
+
+my_other_set.add('Programador')
+print(my_other_set) # Un set no es una estructura ordenada
+
+my_other_set.add('Programador')
+print(my_other_set) # Un set no admite repetidos
