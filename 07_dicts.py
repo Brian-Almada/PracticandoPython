a026928139dd973f4b6c38366709c056f3c0d3f9
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-03 22:04:34 -0300

    Agregando teoría basica sobre diccionarios

---
+++ 07_dicts.py
@@ -0,0 +1,10 @@
+#Dictionaries
+#Es una estructura de datos que nos permite guardar parejas de claves y valores
+
+my_dict = dict()
+my_other_dict = {}
+
+print(type(my_dict))
+print(type(my_other_dict))
+
+my_other_dict = {"Name":"Brian", "Surname":"Almada", "Age":32, 1:"Python" }
