0ffa2f1d9e9a183f6ced35d22fa0a26123bf53c3
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-03 22:18:18 -0300

    Agregando mas teoría sobre diccionarios

--- 07_dicts.py
+++ 07_dicts.py
@@ -8,3 +8,25 @@ print(type(my_dict))
 print(type(my_other_dict))
 
 my_other_dict = {"Name":"Brian", "Surname":"Almada", "Age":32, 1:"Python" }
+
+my_dict = {
+    "Nombre":"Brian",
+    "Apellido":"Almada",
+    "Edad":32,
+    "Lenguajes": {"Python", "JavaScript"},
+    1:"Pastelero",
+    2:"Programador"
+}
+
+print(my_other_dict)
+print(my_dict)
+
+print(my_dict["Nombre"])
+my_dict["Nombre"] = "Alexis"
+print(my_dict)
+
+my_dict["Calle"] = "Mauricio Yadarola 2204"
+print(my_dict)
+
+print("Alexis" in my_dict) # Da False porque in busca keys no values
+print("Nombre" in my_dict)
