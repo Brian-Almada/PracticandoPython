61fc25559a745fe596d2687d9c395ba02de6f887
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-07 17:18:53 -0300

    Otro ejemplo de condicional pero con elif

--- 08_conditionals.py
+++ 08_conditionals.py
@@ -32,3 +32,13 @@ if my_condition > 10 and my_condition < 20:
     print("Es mayor que 10 y menor que 20")
 else:
     print("Es menor o igual que 10 o mayor o igual que 20")
+
+
+my_condition = 5 * 5
+
+if my_condition > 10 and my_condition < 20:
+    print("Es mayor que 10 y menor que 20")
+elif my_condition == 25:
+    print("Es igual a 25")
+else:
+    print("Es menor o igual que 10 o mayor o igual que 20")
