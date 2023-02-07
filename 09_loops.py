2e98230dc579c70e6aeda17080594f343e10cc31
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-07 18:06:10 -0300

    agregando comentario a modo de título sobre el while con if

--- 09_loops.py
+++ 09_loops.py
@@ -10,9 +10,13 @@ while my_condition < 10:
     my_condition += 2 # Con esta línea le ponemos un límite ya que vamos sumando hasta que en un momento sea mayor que diez y el condicional no se cumple y por tanto se detenga su ejecución
 
 
+# While con if
 while my_condition < 20:
     my_condition += 1
     if my_condition == 15:
         print("Se ejecuta el if")
 
     print(my_condition)
+
+
+
