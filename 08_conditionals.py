fad66fe98c90405e45fa0b45a748f4bda08d7088
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-06 23:26:57 -0300

    más teoría de condicionales, if con else

--- 08_conditionals.py
+++ 08_conditionals.py
@@ -20,3 +20,8 @@ my_condition = 5 * 2
 
 if my_condition == 11:
     print("Se ejecutara la condición si es verdadera") # Esta ejecución no se va a realizar porque 11 no cumple con la condición ya que decimos "Sí la variable my_condition es igual a 11 (el resultado de la multiplicación) entonces ejecutá el siguiente programa" Cómo 5 * 2 no es 11 sino 10, entonces la condición no se cumple. Por lo tanto el programa no se ejecuta a menos que cambiemos la condición y le digamos que if my_condition == 10
+
+if my_condition > 10:
+    print("Es mayor que 10")
+else: #Con el else le decimos al condicional que si no se cumple de una forma se cumpla de esta manera
+    print("Es menor o igual que 10")
