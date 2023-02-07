8fc7c765f6d092ae2b7eb27cf0485321f2fda2e0
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-07 17:55:26 -0300

    teoría básica sobre el while

--- 09_loops.py
+++ 09_loops.py
@@ -1,1 +1,10 @@
 #Loops
+#Es un mecanismo para poder iterar
+
+# While
+
+my_condition = 0
+
+while my_condition < 10:
+    print(my_condition) #Si hacemos el programa hasta esta línea se generarpá un bucle infinito
+    my_condition += 2 # Con esta línea le ponemos un límite ya que vamos sumando hasta que en un momento sea mayor que diez y el condicional no se cumple y por tanto se detenga su ejecución
