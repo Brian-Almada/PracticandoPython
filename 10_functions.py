6f7dc07f5eac0d7c2e8185ce6b4fad02cbb7fc6a
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-08 19:21:04 -0300

    Teoría básica de funciones

--- 10_functions.py
+++ 10_functions.py
@@ -1,1 +1,20 @@
 # Functions
+
+def my_function():
+    print("Esto es una función")
+
+my_function()
+
+def sum_two_values(first_number, second_number):
+    print(first_number + second_number)
+
+sum_two_values(34, 123)
+sum_two_values('5', '0')
+sum_two_values(1.4, 5.6)
+
+
+def new_funtion(num_1, num_2):
+    return num_1 + num_2
+
+my_result = new_funtion(432, 4274)
+print(my_result)
