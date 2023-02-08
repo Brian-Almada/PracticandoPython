b769e25533680dada26b491c0861ce2250d331cf
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-08 20:41:48 -0300

    Creando una clase con su constructor

--- 11_classes.py
+++ 11_classes.py
@@ -6,3 +6,14 @@ class MyEmptyPerson:
     pass
 
 print(MyEmptyPerson())
+
+# creando clase con su constructor
+
+class Person:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+my_person = Person("Brian", "Almada")
+
+print(f"{my_person.name} {my_person.surname}")
