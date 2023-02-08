5060389a5a056437e65edd94cdb3223b08af1b96
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-08 20:57:11 -0300

    Creando clase con propiedades dentro del constructor

--- 11_classes.py
+++ 11_classes.py
@@ -17,3 +17,13 @@ class Person:
 my_person = Person("Brian", "Almada") # Aquí estamos definiendo las propiedades por parámetro fuera del constructor
 
 print(f"{my_person.name} {my_person.surname}")
+
+# creando clase con propiedades definidas dentro del constructor
+
+class OnePerson:
+    def __init__(self):
+        self.name = "Izan"
+        self.surname = "Almada"
+
+a_person = OnePerson()
+print(f"{a_person.name} {a_person.surname}")
