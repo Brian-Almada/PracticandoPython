566b5a703da9aefe215b3c1b7828b234766167af
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-08 21:10:24 -0300

    agregando funciones a la clase

--- 11_classes.py
+++ 11_classes.py
@@ -33,6 +33,8 @@ print(f"{a_person.name} {a_person.surname}")
 class twoPerson:
     def __init__(self, name, surname):
         self.full_name = f"{name} {surname}" # Aquí se usa una propiedad como variable para almacenar el resto de propiedades
+    def walk(self): # Aquí podemos agregarle funciones a una clase (la cantidad que querramos)
+        print(f"{self.full_name} Está caminando") 
 
 a_two_person = twoPerson("Eluney", "Almada")
 print(a_two_person.full_name)
