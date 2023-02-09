9e3a6ad9491c226734bb8b1d02760264b7e3be60
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-09 19:53:59 -0300

    try except

--- 12_exceptions.py
+++ 12_exceptions.py
@@ -7,4 +7,15 @@ number_two = "1"
 
 print(number_one + number_two)
 
+# try except
 
+number_tree = 5
+number_four = 1
+
+number_four = "1"
+
+try:
+    print(number_tree + number_four)
+    print("No se ha producido un error")
+except:
+    print("Se ha producido un error")
