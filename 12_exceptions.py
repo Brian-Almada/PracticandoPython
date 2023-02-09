c72622e42166f0c3eee26482078ff0cf37249468
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-09 20:07:03 -0300

    manejando errores por su type

--- 12_exceptions.py
+++ 12_exceptions.py
@@ -12,3 +12,16 @@ try:
     print("No se ha producido un error")
 except:
     print("Se ha producido un error")
+
+# capturando errores por su tipo
+
+num_1 = "1"
+num_2 = 2
+
+try:
+    print(num_1 + num_2)
+    print("Se ejecuta el try si el programa va bien")
+except TypeError:
+    print("Se ejecuta si hay un TypeError")
+except ValueError:
+    print("Se ejecuta si hay un ValueError")
