9a2862da8ad7ff33cbfdf96024a58e75d9366af5
Brian Almada <brianalexisalmada84@gmail.com>
2023-02-08 21:35:10 -0300

    corrigiendo error de espacio en línea 37

--- 11_classes.py
+++ 11_classes.py
@@ -34,7 +34,7 @@ class twoPerson:
     def __init__(self, name, surname):
         self.full_name = f"{name} {surname}" # Aquí se usa una propiedad como variable para almacenar el resto de propiedades
     def walk(self): # Aquí podemos agregarle funciones a una clase (la cantidad que querramos)
-        print(f"{self.full_name} Está caminando") 
+        print(f"{self.full_name} Está caminando")
 
 a_two_person = twoPerson("Eluney", "Almada")
 print(a_two_person.full_name)
